7c9e4f851857d67ad857054c2ef38a1efff6fcf8
devuri <devuriko@gmail.com>
2020-08-24 21:47:45 -0500

    0.4.3 Fix Class "not found"

--- api-featured-image.php
+++ api-featured-image.php
@@ -11,7 +11,7 @@
  * Plugin Name:       WP API Featured Media Source
  * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
  * Description:       This plugin will add the featured image src url field to the WordPress Rest API.
- * Version:           0.4.2
+ * Version:           0.4.3
  * Requires at least: 3.4
  * Requires PHP:      5.6
  * Author:            SwitchWebdev.com
@@ -28,7 +28,7 @@
 	}
 
  	# plugin directory
-	define("APIFI_VERSION", '0.4.2');
+	define("APIFI_VERSION", '0.4.3');
 
  	# plugin directory
 	define("APIFI_DIR", dirname(__FILE__));
@@ -52,7 +52,7 @@
 	 *
 	 * TODO add option to change the image size for output ('thumbnail', 'medium', 'large')
 	 */
- 	$src_field = new SwitchWebdev\AddFeaturedImageSrc( get_option('wpfeatured_media_src_post_types') , 'large'  );
+ 	$src_field = new SimFeaturedMediaSrc\addFeaturedImageSrc( get_option('wpfeatured_media_src_post_types') , 'large'  );
 
 	/**
 	 * add the src url field
