603b06533ee9519f170bc5b657c8e3a3e229bdcb
devuri <devuriko@gmail.com>
2020-09-27 12:52:49 -0500

    0.6.4

--- api-featured-image.php
+++ api-featured-image.php
@@ -11,7 +11,7 @@
  * Plugin Name:       WP REST API Featured Media Source
  * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
  * Description:       This plugin will add the featured image src url field to the WordPress Rest API.
- * Version:           0.6.3
+ * Version:           0.6.4
  * Requires at least: 3.4
  * Requires PHP:      5.6
  * Author:            SwitchWebdev.com
@@ -28,7 +28,7 @@
 	}
 
  	# plugin directory
-	define("APIFI_VERSION", '0.6.3');
+	define("APIFI_VERSION", '0.6.4');
 
  	# plugin directory
 	define("APIFI_DIR", dirname(__FILE__));
